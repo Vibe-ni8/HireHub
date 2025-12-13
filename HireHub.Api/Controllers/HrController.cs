0c4b9609557a68a6073e28ec986d48dbe43b9638
Vibe-ni8 <vigneshmalliga02@gmail.com>
2025-12-13 12:31:16 +0530

    Added Controllers and Services Skeleton

---
+++ HireHub.Api/Controllers/HrController.cs
@@ -0,0 +1,39 @@
+﻿using HireHub.Core.Data.Interface;
+using HireHub.Core.Service;
+using HireHub.Core.Utils.UserProgram.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HireHub.Api.Controllers;
+
+public class HrController : ControllerBase
+{
+    private readonly UserService _userService;
+    private readonly IUserProvider _userProvider;
+    private readonly RepoService _repoService;
+    private readonly ITransactionRepository _transactionRepository;
+    private readonly ILogger<UserController> _logger;
+
+    public HrController(UserService userService, IUserProvider userProvider,
+        RepoService repoService, ITransactionRepository transactionRepository,
+        ILogger<UserController> logger)
+    {
+        _userService = userService;
+        _userProvider = userProvider;
+        _repoService = repoService;
+        _transactionRepository = transactionRepository;
+        _logger = logger;
+    }
+
+
+    #region Get API's
+
+
+
+    #endregion
+
+    #region Post API's
+
+
+
+    #endregion
+}
---
+++ HireHub.Core/Service/MentorService.cs
@@ -0,0 +1,38 @@
+﻿using HireHub.Core.Data.Interface;
+using Microsoft.Extensions.Logging;
+
+namespace HireHub.Core.Service;
+
+public class MentorService
+{
+    private readonly IUserRepository _userRepository;
+    private readonly ISaveRepository _saveRepository;
+    private readonly ILogger<CandidateService> _logger;
+
+    public MentorService(IUserRepository userRepository,
+        ISaveRepository saveRepository, ILogger<CandidateService> logger)
+    {
+        _userRepository = userRepository;
+        _saveRepository = saveRepository;
+        _logger = logger;
+    }
+
+
+    #region Query Services
+
+
+
+    #endregion
+
+    #region Command Services
+
+
+
+    #endregion
+
+    #region Private Methods
+
+
+
+    #endregion
+}
